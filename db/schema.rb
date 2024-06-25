7c5572c7f954236fc80aa79abed43933af2db550
jarriemorrow <otz2307@gmail.com>
2024-06-25 15:02:08 +0900

    postテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_100637) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_25_023401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.string "recipe_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
