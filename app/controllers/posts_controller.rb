34b3a3a0259f5dde6d55624101535c93d144c075
jarriemorrow <otz2307@gmail.com>
2024-06-27 17:57:19 +0900

    メニュー登録テスト実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+    @menus = Menu.all
   end
 
-  def show
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      update_tags_and_menus
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      @menus = Menu.all
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, menu_ids: [])
+  end
+
+  def update_tags_and_menus
+    @post.menus = Menu.where(id: params[:post][:menu_ids])
   end
 end
