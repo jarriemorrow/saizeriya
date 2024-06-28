83e13f2c4be799b77afe63820d383c12502dc9cf
jarriemorrow <otz2307@gmail.com>
2024-06-28 20:38:41 +0900

    post-newにタグ投稿を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @menus = Menu.all
+    @tags = Tag.all
   end
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      @post.menus = Menu.where(id: params[:post][:menu_ids])
+      update_menus_and_tags
       redirect_to @post, notice: 'Post was successfully created.'
     else
       @menus = Menu.all
+      @tags = Tag.all
       render :new
     end
   end
@@ -26,6 +28,11 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:body, menu_ids: [])
+    params.require(:post).permit(:body, menu_ids: [], tag_ids: [])
+  end
+
+  def update_menus_and_tags
+    @post.menus = Menu.where(id: params[:post][:menu_ids])
+    @post.tags = Tag.where(id: params[:post][:tag_ids])
   end
 end
