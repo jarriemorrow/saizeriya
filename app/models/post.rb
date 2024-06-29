153ee7db7e01dbd2e2cda18bb0c8678124a4bcb3
jarriemorrow <otz2307@gmail.com>
2024-06-29 22:33:06 +0900

    postモデル合計金額計算メソッド作成、post-showに表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   has_many :menus, through: :post_menus
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+  #メニューの合計値計算
+  def total_price
+    menus.sum(:price)
+  end
 end
