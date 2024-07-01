4cc844925bec1e41c3f5a66b3fa314530ab08b0f
jarriemorrow <otz2307@gmail.com>
2024-07-01 10:01:03 +0900

    ユーザーモデルにown?追加、post-show編集ページリンク追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
 
+  def own?(object)
+    object.user_id == id
+  end
 end
