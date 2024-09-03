02d147bfcb97b336fb5f1db3ccddfd165de6278d
jarriemorrow <otz2307@gmail.com>
2024-09-03 21:14:33 +0900

    pairing,postモデル関連付け

---
+++ app/models/pairing_menu.rb
@@ -0,0 +1,4 @@
+class PairingMenu < ApplicationRecord
+  belongs_to :post
+  belongs_to :menu
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,19 @@ class Post < ApplicationRecord
   has_many :post_menus, dependent: :destroy
   has_many :course_menus, dependent: :destroy
   has_many :course_related_menus, through: :course_menus, source: :menu
+  has_many :arrange_menus, dependent: :destroy
   has_many :arrange_related_menus, through: :arrange_menus, source: :menu
+  has_many :pairing_menus, dependent: :destroy
+  has_many :pairing_related_menus, through: :pairing_menus, source: :menu
 
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   has_many :likes, dependent: :destroy
 
+  accepts_nested_attributes_for :post_menus, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :course_menus, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :arrange_menus, allow_destroy: true, reject_if: :all_blank
-  accepts_nested_attributes_for :post_menus, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :pairing_menus, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :post_tags, allow_destroy: true
 
   # メニューの合計値計算
