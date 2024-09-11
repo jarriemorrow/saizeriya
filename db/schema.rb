90dbdef537ba8b42706d2dec2b8bce2edaa995f5
jarriemorrow <otz2307@gmail.com>
2024-09-11 15:22:56 +0900

    postsテーブルに:images追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_03_080250) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_11_062002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_03_080250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.json "images"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
