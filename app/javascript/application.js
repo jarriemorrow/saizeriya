649d289051c08470212e41042f66ff39de931ebc
jarriemorrow <otz2307@gmail.com>
2024-09-13 19:26:25 +0900

    レスポンシブ時の画像表示調整

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,24 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 import Cocooned from '@notus.sh/cocooned'
 Cocooned.start()
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal', // 水平スライド
+  loop: true,
+  slidesPerView: 1, // 1枚ずつ表示
+  autoHeight: true, // 画像に合わせて高さを自動調整
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
