c87e9b615d5ef247a070ae69b2d562770c27c613
jarriemorrow <otz2307@gmail.com>
2024-09-15 09:56:20 +0900

    複数呼び出し部分を

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
   skip_before_action :require_login, only: %i[index show]
   before_action :set_post, only: %i[edit update destroy]
+
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).includes(:user, :likes).order(created_at: :desc).page(params[:page])
+    @posts = @q.result(distinct: true)
+               .includes(:user, :likes)
+               .order(created_at: :desc)
+               .page(params[:page])
   end
 
   def new
     @post = Post.new
-    @post.post_tags.build
-    @post.course_menus.build
-    @post.arrange_menus.build
-    @post.pairing_menus.build
+    build_associations(@post)
     @course_sections = CourseSection.all
   end
 
@@ -20,22 +21,26 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
+      build_associations(@post) # renderに戻った際に、associationsがnilにならないようにした
+      @course_sections = CourseSection.all
       render :new
     end
   end
 
   def show
-    @post = Post.includes(:post_tags, :tags, :likes)
-    .left_joins(:course_menus, :arrange_menus, :pairing_menus)
-    .find(params[:id])
+    @post = Post.includes(:post_tags, :tags, :likes, :course_menus, :arrange_menus, :pairing_menus)
+                .find(params[:id])
   end
   
-  def edit; end
+  def edit
+    build_associations(@post)
+  end
 
   def update
     if @post.update(post_params)
       redirect_to @post, notice: '投稿が更新されました。'
     else
+      build_associations(@post)
       render :edit
     end
   end
@@ -47,14 +52,24 @@ class PostsController < ApplicationController
 
   def likes
     @q = current_user.like_posts.ransack(params[:q])
-    @like_posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+    @like_posts = @q.result(distinct: true)
+                    .includes(:user)
+                    .order(created_at: :desc)
+                    .page(params[:page])
   end
 
   private
 
   def set_post
-    @post = current_user.posts.find(params[:id])
+    @post = current_user.posts.includes(:course_menus, :arrange_menus, :pairing_menus).find(params[:id])
     @course_sections = CourseSection.all
+  end
+
+  def build_associations(post)
+    post.post_tags.build if post.post_tags.empty?
+    post.course_menus.build if post.course_menus.empty?
+    post.arrange_menus.build if post.arrange_menus.empty?
+    post.pairing_menus.build if post.pairing_menus.empty?
   end
 
   def post_params
@@ -62,10 +77,10 @@ class PostsController < ApplicationController
       :recipe_name,
       :body, 
       images: [],
-      post_tags_attributes: [ :id, :tag_id ], 
-      course_menus_attributes: [ :id, :menu_id, :course_section_id, :_destroy ],
-      arrange_menus_attributes: [ :id, :menu_id, :_destroy ],
-      pairing_menus_attributes: [ :id, :menu_id, :_destroy ]
+      post_tags_attributes: [:id, :tag_id], 
+      course_menus_attributes: [:id, :menu_id, :course_section_id, :_destroy],
+      arrange_menus_attributes: [:id, :menu_id, :_destroy],
+      pairing_menus_attributes: [:id, :menu_id, :_destroy]
     )
   end
 end
