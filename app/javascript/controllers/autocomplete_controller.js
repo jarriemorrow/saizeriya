752d6e8f8990f21e784e478ec17bf1dac80449bb
jarriemorrow <otz2307@gmail.com>
2024-09-15 20:49:41 +0900

    表示結果調整

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -7,17 +7,33 @@ export default class extends Controller {
     if (!this.hasResultsTarget) {
       console.error('Missing target element "results" for "autocomplete" controller');
     }
+    this.resultsTarget.classList.add('hidden');  // 初期状態で非表示にする
   }
 
   async search(event) {
-    const query = this.inputTarget.value;
+    const query = this.inputTarget.value.trim();
+
+    if (query === "") {
+      this.clearResults();
+      return;
+    }
+
     const response = await fetch(`/search_menus?query=${encodeURIComponent(query)}`);
-    const results = await response.json();
-    this.displayResults(results);
+    if (response.ok) {
+      const results = await response.json();
+      this.displayResults(results);
+    } else {
+      console.error("Failed to fetch search results.");
+    }
   }
 
   displayResults(results) {
-    this.resultsTarget.innerHTML = '';
+    this.clearResults();
+
+    if (results.length === 0) {
+      return;
+    }
+
     results.forEach(result => {
       const listItem = document.createElement('li');
       listItem.textContent = result.menu_name;
@@ -29,11 +45,19 @@ export default class extends Controller {
 
       this.resultsTarget.appendChild(listItem);
     });
+
+    // 候補リストを表示
+    this.resultsTarget.classList.remove('hidden');
   }
 
   selectResult(result) {
     this.inputTarget.value = result.menu_name;
     this.hiddenInputTarget.value = result.id;
+    this.clearResults();
+  }
+
+  clearResults() {
     this.resultsTarget.innerHTML = '';
+    this.resultsTarget.classList.add('hidden');  // 検索結果がない場合に非表示
   }
 }
