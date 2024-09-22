1d41393ddc9b5ecd2974d3da8f2f254f25627019
jarriemorrow <otz2307@gmail.com>
2024-09-22 13:54:46 +0900

    password_reset用カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_20_090409) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_22_045132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,6 +110,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_20_090409) do
     t.string "salt"
     t.string "avatar"
     t.integer "role", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "arrange_menus", "menus"
