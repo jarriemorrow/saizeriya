2f79de50236c615bbbb36a00f35d1bd72ebc482a
jarriemorrow <otz2307@gmail.com>
2024-09-22 14:38:50 +0900

    user-reset_passwordカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -114,7 +114,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_22_045132) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "arrange_menus", "menus"
