931faf936c5187617d92f459b76ff82675138047
jarriemorrow <otz2307@gmail.com>
2024-09-23 12:15:43 +0900

    default_from:変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,6 @@
 class UserMailer < ApplicationMailer
-  default from: ''
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.reset_password_email.subject
-  #
+  default from: '312koryaku@gmail.com'
+
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
