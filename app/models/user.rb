0c99d64ad982d24aad84139ad4f9f1da82f43e3e
jarriemorrow <otz2307@gmail.com>
2024-09-26 11:38:10 +0900

    authentications関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
+  
   validates :password, length: { minimum: 3}, if: -> { new_record? || changes[:crypted_password]}
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
