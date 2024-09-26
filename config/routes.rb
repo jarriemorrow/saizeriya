9d67393786a1aa6932d5c75f131c88c27073edd5
jarriemorrow <otz2307@gmail.com>
2024-09-26 11:39:46 +0900

    oauthルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" 
+  get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
+    
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get 'posts/index'
@@ -17,4 +21,5 @@ Rails.application.routes.draw do
   resources :likes, only: %i[create destroy]
   get 'search_menus', to: 'menus#search'
   resources :password_resets, only: [:create, :edit, :update, :new]
+
 end
