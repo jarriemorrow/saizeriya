65dd8ad86f551bd69c64374d4416fe71365b3e10
jarriemorrow <otz2307@gmail.com>
2024-09-26 14:55:31 +0900

    google認証機能

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -159,7 +159,7 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:google]
   config.google.key = ENV["GOOGLE_CLIENT_ID"]
   config.google.secret = ENV["GOOGLE_CLIENT_SECRET"]
-  config.google.callback_url = "https://saizeri.fly.dev/oauth/callback?provider=google"
+  config.google.callback_url = Settings.sorcery[:google_callback_url]
   config.google.user_info_mapping = {:email => "email", :name => "name"}
   # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
