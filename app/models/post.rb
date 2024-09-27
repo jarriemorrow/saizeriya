8bf99a34c1da02e22ba1fe36b41fdfb034c44c89
jarriemorrow <otz2307@gmail.com>
2024-09-27 13:14:43 +0900

    マルチ検索機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -44,6 +44,21 @@ class Post < ApplicationRecord
     ["course_related_menus", "arrange_related_menus", "pairing_related_menus", "tags"]
   end
 
+  # マルチ検索機能
+  def self.search_multiple_keywords(keywords)
+    return all if keywords.blank?
+    ransack_conditions = keywords.map do |keyword|
+      {
+        recipe_name_or_body_or_course_related_menus_menu_name_or_arrange_related_menus_menu_name_or_pairing_related_menus_menu_name_or_tags_name_cont: keyword
+      }
+    end
+  
+    # AND条件の検索
+    ransack_query = ransack(ransack_conditions.reduce(&:merge))
+    # 結果を返す
+    ransack_query.result(distinct: true)
+  end
+  
   def first_image_url
     if images.present? && images[0].present?
       images[0].url
