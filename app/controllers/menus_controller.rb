5a03d09cbe2ff951bdc6d9a09f18b6b8ae616650
jarriemorrow <otz2307@gmail.com>
2024-10-02 08:18:14 +0900

    ログイン不要処理

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,5 @@
 class MenusController < ApplicationController
+  skip_before_action :require_login, only: [:search]
   def search
     query = params[:query]
     @menus = Menu.where('menu_name LIKE ?', "%#{query}%").limit(10)
