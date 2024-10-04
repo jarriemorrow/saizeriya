5afc1f7527b752e69bc0d2fe3468b2d87a1643c4
jarriemorrow <otz2307@gmail.com>
2024-10-04 17:51:08 +0900

    validation-menu_no,uniquness表記削除

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -8,7 +8,7 @@ class Menu < ApplicationRecord
 
   validates :menu_name, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :menu_no, presence: true, uniqueness: true
+  validates :menu_no, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
     ["menu_name", "price", "menu_no"]
