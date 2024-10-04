22ff173634b034715be8cfc184ac270e769acac2
jarriemorrow <otz2307@gmail.com>
2024-10-04 18:09:48 +0900

    has_manyにdependent設定

--- app/models/course_section.rb
+++ app/models/course_section.rb
@@ -1,5 +1,5 @@
 class CourseSection < ApplicationRecord
-  has_many :course_menus
+  has_many :course_menus, dependent: :destroy
   has_many :menus, through: :course_menus
   has_many :posts, through: :course_menus
 end
