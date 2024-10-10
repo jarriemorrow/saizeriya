bffbd9b13d576e5ddb1a6e5816e32d8a0aea9b93
jarriemorrow <otz2307@gmail.com>
2024-10-10 16:15:12 +0900

    test-いいね機能テスト追加

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,13 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get likes_create_url
+  setup do
+    @post = posts(:one)
+    @user = users(:one)
+    @like = Like.find_by(user: @user, post: @post)
+    @like&.destroy
+    log_in_as(@user)
+  end
+
+  test "should create like" do
+    assert_difference 'Like.count', 1 do
+      post likes_url, params: { post_id: @post.id }, xhr: true
+    end
     assert_response :success
   end
 
-  test "should get destroy" do
-    get likes_destroy_url
+  test "should destroy like" do
+    @like = Like.create(user: @user, post: @post) 
+    assert_difference('Like.count', -1) do
+      delete like_url(@like), xhr: true
+    end
     assert_response :success
   end
 end
