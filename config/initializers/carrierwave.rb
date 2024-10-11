e7d0347ab085c17f78ba914d28781aee70ffade6
jarriemorrow <otz2307@gmail.com>
2024-10-11 12:03:30 +0900

    test環境でローカルファイルシステム使用に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_directory = 'jarrie-portfolio'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
-    region: 'ap-northeast-1',
-    path_style: true
-  }
-  
-  config.fog_public = false # バケットがACLをサポートしない場合
+  if Rails.env.production? || Rails.env.development?
+    config.storage = :fog
+    config.fog_directory = 'jarrie-portfolio'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+    config.fog_public = false
+  elsif Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+  end
 end
-
