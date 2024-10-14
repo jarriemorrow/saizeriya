642414a5a613074c4b7002cd5cf9dd3927543f01
jarriemorrow <otz2307@gmail.com>
2024-10-14 10:54:23 +0900

    production-host追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "saizeri.fly.dev"
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "saizeri.fly.dev"
+  config.hosts << "312koryaku.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
