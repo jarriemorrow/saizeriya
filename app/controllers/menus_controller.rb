075181e8bc218dd06fd89e0219cc54cc007dd7c4
jarriemorrow <otz2307@gmail.com>
2024-10-16 12:24:41 +0900

    where-status修正

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,7 +2,7 @@ class MenusController < ApplicationController
   skip_before_action :require_login, only: [:search]
   def search
     query = params[:query]
-    @menus = Menu.where(menus_status: :active).where('menu_name LIKE ?', "%#{query}%").limit(10)
+    @menus = Menu.where(status: :active).where('menu_name LIKE ?', "%#{query}%").limit(10)
     render json: @menus
   end
 end
