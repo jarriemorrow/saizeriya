9827be5647d001ae945cc0a072295c91951e58a3
jarriemorrow <otz2307@gmail.com>
2024-10-20 10:42:12 +0900

    投稿済みのメニューから検索できる仕様を追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,8 +1,17 @@
 class MenusController < ApplicationController
-  skip_before_action :require_login, only: [:search]
+  skip_before_action :require_login, only: [:search, :search_post]
   def search
     query = params[:query]
     @menus = Menu.where(status: :active).where('menu_name LIKE ?', "%#{query}%").limit(10)
     render json: @menus
+  end
+
+  def search_post
+    query = params[:query]
+    @menus = Menu.joins(:arrange_menus, :course_menus, :pairing_menus)
+                 .where(menu_status: :active)
+                 .distinct
+                 .where("menus.name LIKE ?", "%#{params[:query]}%")
+    render json: @menus.pluck(:name)
   end
 end
